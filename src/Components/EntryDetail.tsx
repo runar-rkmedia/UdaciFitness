7a2e3ecb666353b002af9e41edfc963558b81fc2
Runar Kristoffersen <public@rkmedia.no>
2017-12-12 22:32:15 +0100

    Add EntryDetail.

--- src/Components/EntryDetail.tsx
+++ src/Components/EntryDetail.tsx
@@ -1,18 +1,83 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import * as React from 'react'
+import { View, StyleSheet } from 'react-native'
 import { NavigationScreenConfigProps } from 'react-navigation'
+import { connect, Dispatch } from 'react-redux'
+import { timeToString, getDailyReminderValue, white, removeEntry, DailyReminder } from '../utils/'
+import { MetricCard } from './'
+import { TextBtn } from './'
+import { addEntry } from '../actions'
+import { StoreState, Entry } from '../store'
 
-interface Props {}
+interface Props extends NavigationScreenConfigProps {
+  remove: () => any
+  goBack: () => any
+  entryID: string
+  metrics: Entry | DailyReminder
+}
 
-interface State {}
+class EntryDetailC extends React.Component<IConnectProps> {
+  static navigationOptions = ({ navigation }: any) => {
+    const { entryID } = navigation.state.params
 
-type CProps = NavigationScreenConfigProps & Props
-export class EntryDetail extends React.Component<CProps, State> {
+    const year = entryID.slice(0, 4)
+    const month = entryID.slice(5, 7)
+    const day = entryID.slice(8)
+
+    return {
+      title: `${month}/${day}/${year}`
+    }
+  }
+  reset = () => {
+    const { remove, goBack, entryID } = this.props
+
+    remove()
+    goBack()
+    removeEntry(entryID)
+  }
+  shouldComponentUpdate(nextProps: Props) {
+    return nextProps.metrics !== null && !(nextProps.metrics as DailyReminder).today
+  }
   render() {
+    const { metrics, entryID } = this.props
     return (
-      <View>
-        <Text>Entry Detail – {this.props.navigation.state.params.entryID}</Text>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} date={entryID} />
+        <TextBtn btnStyle={{ margin: 20 }} onPress={this.reset}>
+          RESET
+        </TextBtn>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+})
+
+import { } from '../actions'
+const connectCreator = connect(
+  (state: StoreState, { navigation }: Props) => {
+    const { entryID } = navigation.state.params
+    return {
+      entryID,
+      metrics: state.entries[entryID]
+    }
+  },
+  (dispatch: Dispatch<{}>, { navigation }: Props) => {
+    const { entryID } = navigation.state.params
+    return {
+      remove: () => dispatch(addEntry({
+        [entryID]: timeToString() === entryID
+          ? getDailyReminderValue()
+          : null
+      })),
+      goBack: () => navigation.goBack(),
+    }
+  },
+)
+type IConnectProps = typeof connectCreator.allProps
+export const EntryDetail = connectCreator(EntryDetailC)
